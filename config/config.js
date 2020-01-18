6907aa9f01988d94c2de1f7424128a72b7c9304a
Jorden Ragels <jordenragels3@gmail.com>
2020-01-18 10:19:26 -0700

    env file and routes/db

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-{
+module.exports = {
     "development": {
-        "username": "root",
-        "password": "password",
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
         "database": "milestones_db",
         "host": "127.0.0.1",
         "dialect": "mysql",
@@ -12,8 +12,8 @@
         }
     },
     "production": {
-        "username": "root",
-        "password": "password",
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
         "database": "milestones_db",
         "host": "127.0.0.1",
         "dialect": "mysql",
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const bookController = require("./book.controller");
 const authController = require("./auth.controller");
+const cognitiveController = require("./cognitive.controller");
 
 // API Routes
-router.use("/api/books", bookController);
+router.use("/api/cognitive", cognitiveController);
+
+router.use("/api/book", bookController);
 
 // Auth Routes
 router.use("/auth", authController)
