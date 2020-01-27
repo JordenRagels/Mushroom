a0ac1fe7aca420774e0b1daf4fbd857b0f2ae76d
mcleddy <maryleddy1@gmail.com>
2020-01-27 11:22:21 -0700

    css for milestones

--- client/src/components/Navigations/index.jsx
+++ client/src/components/Navigations/index.jsx
@@ -1,53 +1,37 @@
 import React from "react"
 import { Link } from 'react-router-dom'
 import './navigations.css'
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 function NavigationsComponent() {
     return (
-        <nav className="navigations">
-                <ul className="nav">
-                <li className="nav-item">
-                        <Link to="/" className="nav-link">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/cognitive" className="nav-link">
-                            Cognitive Milestones
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/motor" className="nav-link">
-                            Motor Milestones
-						</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/social" className="nav-link">
-                            Social Milestones
-						</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/verbal" className="nav-link">
-                            Verbal Milestones
-						</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/percentile" className="nav-link">
-                            Percentile Calculator
-						</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/eating" className="nav-link">
-                            Eating Chart
-						</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/sleeping" className="nav-link">
-                           Sleeping Chart
-						</Link>
-                    </li>
-                </ul>
-        </nav>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Mushroom</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/home">Home</Nav.Link>
+      <NavDropdown title="Milestones" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/cognitive">Cognitive</NavDropdown.Item>
+        <NavDropdown.Item href="/motor">Motor</NavDropdown.Item>
+        <NavDropdown.Item href="/social">Social</NavDropdown.Item>
+        <NavDropdown.Item href="/verbal">Verbal</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="Sleeping & Eating Recomendations" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/sleeping">Sleeping</NavDropdown.Item>
+        <NavDropdown.Item href="/eating">Eating</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="Percentile Calculators" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/weightCalc">Weight Percentile Calculator</NavDropdown.Item>
+        <NavDropdown.Item href="/lengthCalc">Length Percentile Calculator</NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown title="Parent Resources" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/doctorSearch">Find a Pediatrician</NavDropdown.Item>
+        <NavDropdown.Item href="/meditation">Meditation</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     )
 }
 
--- client/src/pages/Milestones/Social/progress.js
+++ client/src/pages/Milestones/Social/progress.js
@@ -1,28 +1,2 @@
-import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
-
-
-const data = {
-    labels: ['Red'],
-    datasets: [{
-        data: [10, 20, 30],
-        backgroundColor:'red'
-    }
-    ],
-}
-
-
-class ProgChart extends React.Component {
-    
-
-    render() {
-        return (
-            <Doughnut data={data} />
-        )
-    };
-}
-
-
-export default ProgChart;
 
 
