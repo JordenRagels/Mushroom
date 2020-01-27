81be14efdcbff60660d39f4e393053612ac605f0
Jorden Ragels <jordenragels3@gmail.com>
2020-01-26 23:09:46 -0700

    routing

---
+++ client/src/pages/Home/home.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Header } from '../../components';
+import "./home.css";
+
+
+
+<button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button>
